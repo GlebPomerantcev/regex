558b5ba2ec3c0d046755843639cb610ea4235c84
GlebPomerantcev <43440807+GlebPomerantcev@users.noreply.github.com>
2018-11-22 13:38:42 +0300

    Add files via upload

---
+++ regexe/src/L1.java
@@ -0,0 +1,85 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class L1 {
+
+    public static void main(String[] args) {
+
+//        Pattern p = Pattern.compile("l");
+//        Matcher m = p.matcher("We all love GWTP");
+//        while (m.find()){
+//            System.out.println(m.start());
+//        }
+
+//        Pattern p = Pattern.compile("1+1=2");
+//        Matcher m = p.matcher("1+1=2 2+2=4");
+//        while (m.find()){
+//            System.out.println(m.start()+" "+m.end());
+//        }
+
+//        Pattern p = Pattern.compile("cat");
+//        Matcher m = p.matcher("He captured a catfish for his cat");
+//        while (m.find()){
+//            System.out.println(m.start());
+//        }
+
+//        Pattern p = Pattern.compile("[a-g]");
+//        Matcher m = p.matcher("We all love GWTP!");
+//        while(m.find()){
+//            System.out.println(m.start());
+//        }
+
+//        Pattern p = Pattern.compile("gr[ae]y");
+//        Matcher m = p.matcher("gray");
+//        while(m.find()){
+//            System.out.println(m.start() + " - " + m.group() );
+//        }
+
+//        Pattern p = Pattern.compile("G[^W]");
+//        Matcher m = p.matcher("We all love GWTP");
+//        while(m.find()){
+//            System.out.println(m.start());
+//        }
+
+//        Pattern p = Pattern.compile("\\w");
+//        Matcher m = p.matcher("We all love GWTP!!1!");
+//        while(m.find()){
+//            System.out.println(m.start()+ " - " + m.group());
+//        }
+
+//        Pattern p = Pattern.compile("\\d\\d.\\d\\d.\\d\\d");
+//        Matcher m = p.matcher("");
+//        while (m.find()){
+//            System.out.println(m.group());
+//        }
+
+//        Pattern p = Pattern.compile("\".*?\"");
+//        Matcher m = p.matcher("bla bla \"wow\" bla \"wow\" bla");
+//        while (m.find()){
+//            System.out.println(m.group());
+//        }
+
+//        Pattern p = Pattern.compile("\\bbla\\b");
+//        Matcher m = p.matcher("bla bad blabla ball bla");
+//        while (m.find()){
+//            System.out.println(m.group());
+//        }
+
+//        Pattern p = Pattern.compile("^a");
+//        Matcher m = p.matcher("abc");
+//        while (m.find()){
+//            System.out.println(m.group());
+//        }
+
+//        Pattern p = Pattern.compile("Get|GetValue|Set|SetValue");
+//        Matcher m = p.matcher("SetValue");
+//        while (m.find()) {
+//            System.out.println(m.group());
+//        }
+
+
+
+
+    }
+}
+
---
+++ regexe/src/L2.java
@@ -0,0 +1,20 @@
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class L2 {
+
+    public static void main(String[] args) {
+
+//        Pattern p = Pattern.compile("We all love");
+//        Matcher m = p.matcher("We all love GWTP");
+//        System.out.println(m.lookingAt());
+//        System.out.println(m.matches());
+
+//        Pattern p = Pattern.compile("Dogs");
+//        Matcher m = p.matcher("Dogs say meow!");
+//        System.out.println(m.replaceAll("Cats"));
+
+//        System.out.println("We use Eclipse".replaceAll("We use (Eclipse|NetBean)", "$1 is't as comfortable as Idea"));
+    }
+}
